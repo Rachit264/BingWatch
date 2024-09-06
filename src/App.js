fffc76c2609e3d97b3706579e505c46f66ee486b
Rachit264 <rachit.rmqr@gmail.com>
2024-09-06 16:30:32 +0530

    name change

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App=()=>{
     },[]);
     return(
         <div className='app'>
-            <h1>MovieLand</h1>
+            <h1>Movieji</h1>
             <div className='search'>
                 <input 
                 placeholder='Search for Movies'
